67a156663c96ffd23ba9cb5652adf3fa94d01cb4
Brian Hann <brian.hann@gmail.com>
2018-11-09 10:38:58 -0600

    Genericize component constructors

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
+export { default as Config } from './config';
 import { default as HotspotComponent } from './components/Hotspot.svelte';
 import { default as TooltipComponent } from './components/Tooltip.svelte';
-export { default as Config } from './config';
+
+function genericConstructor(component, args) {
+  const data = {};
+  ['style', 'title', 'content'].forEach(x => {
+    if (args[x]) data[x] = args[x];  
+  })
+
+  return new component({
+    ...args,
+    data,
+    target: document.querySelector('body'),
+  });
+}
 
 export class Hotspot {
   constructor(args) {
-    const data = {}
-    if (args.style) data.style = args.style;
-
-    return new HotspotComponent({
-      ...args,
-      data,
-      target: document.querySelector('body') });
+    return genericConstructor(HotspotComponent, args);
   }
 }
 
 export class Tooltip {
   constructor(args) {
-    const data = {};
-    ['style', 'title', 'content'].forEach(x => {
-      if (args[x]) data[x] = args[x];  
-    })
-
-    return new TooltipComponent({
-      ...args,
-      data,
-      target: document.querySelector('body') });
+    return genericConstructor(TooltipComponent, args);
   }
 }
